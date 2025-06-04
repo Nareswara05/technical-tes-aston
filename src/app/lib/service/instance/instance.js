13b640b7cbffbcc734da157944adafc81899c113
Nareswara Maulana Aletha Alhaq <113566487+Nareswara05@users.noreply.github.com>
2025-06-05 01:08:54 +0700

    feat [NR] add auto remove token if token expired

--- src/app/lib/service/instance/instance.js
+++ src/app/lib/service/instance/instance.js
@@ -11,5 +11,24 @@ const instance = axios.create({
     },
 });
 
+//jika token expired maka otomatis akan menghapus token dari localStorage dan cookies, lalu redirect ke halaman login
+instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response && error.response.status === 401) {
+            
+            setTimeout(() => {
+                // menghapus token dari cookies dan localStorage
+                document.cookie = 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+                localStorage.removeItem('token');
+        
+                window.location.href = '/auth/login';
+            }, 2000); 
+        }
+        return Promise.reject(error);
+    }
+);
+
+
 
 export default instance;
